d2430e99cf9d291497ab8ccb55a595c1cbccf9f9
LAPTOP-6ERV3FT1\britn <britneykhuu@gmail.com>
2023-08-01 00:34:15 -0700

    edit plaer prefab; made basis of firefly script

--- Fireflies/Assets/Scripts/Fireflies.cs
+++ Fireflies/Assets/Scripts/Fireflies.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Fireflies : MonoBehaviour
 {
+    [SerializeField] Transform[] posF;
+    [SerializeField] float spdF =30f;
+    Transform nextPos;
+    
     // Start is called before the first frame update
+    int nextPosI;
     void Start()
     {
-        
+        nextPos = posF[0];//does immediately; will edit later for player action
     }
-
+    //use OnTriggerEnter2D/OnTriggerExit2D for collison bw fireflies and player
     // Update is called once per frame
     void Update()
     {
-        
+        MoveFireflies();
+    }
+    void MoveFireflies()
+    {
+        if(transform.position == nextPos.position)
+        {
+            nextPosI++;
+            if(nextPosI >= posF.Length) 
+            {
+                nextPosI = 0;
+            }
+            nextPos = posF[nextPosI];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPos.position,spdF *Time.deltaTime);
+        }
     }
 }
